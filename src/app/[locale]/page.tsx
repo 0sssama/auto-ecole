f3a4229dc6426dc6693e1c28096309dda2b60f45
Oussama Labrahmi <labrahmioussama@gmail.com>
2023-10-19 19:55:01 +0100

    chore: adding language, editing home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,22 +1,17 @@
-import Link from "next/link";
+"use client";
 
-export default function Home({
-  params: { locale },
-}: {
-  params: {
-    locale: string;
-  };
-}) {
+import Link from "next/link";
+import { useTranslations } from "next-intl";
+
+export default function Home() {
+  const t = useTranslations("Landing");
+
   return (
     <main className="relative flex items-center justify-center min-h-screen gap-4 p-24">
       <Link className="absolute inset-0 z-[3]" href="/dash" />
       <div className="flex flex-col items-center justify-center w-full max-w-screen-xl gap-4 m-auto">
-        <h1 className="text-4xl font-bold text-center">
-          Landing Page Here, Locale: {locale}
-        </h1>
-        <p className="text-sm font-semibold text-slate-500">
-          Click anywhere to continue to dashboard
-        </p>
+        <h1 className="text-4xl font-bold text-center">{t("title")}</h1>
+        <p className="text-sm font-semibold text-slate-500">{t("subtitle")}</p>
       </div>
     </main>
   );
