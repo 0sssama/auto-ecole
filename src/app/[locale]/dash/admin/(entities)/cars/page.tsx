a19dde13be1642bfa38b32e31760c396c20f26e8
Oussama Labrahmi <labrahmioussama@gmail.com>
2023-12-02 00:17:41 +0100

    chore: [AUTO-31] adding inexistent pages as `Under Construction`

---
+++ src/app/[locale]/dash/admin/(entities)/cars/page.tsx
@@ -0,0 +1,12 @@
+export default function Cars() {
+  return (
+    <div className="w-full min-h-[300px] grid place-items-center">
+      <div className="flex flex-col items-center gap-1">
+        <h1 className="text-2xl font-bold">Coming soon.</h1>
+        <p className="text-sm text-center opacity-70">
+          Under construction {":)"}
+        </p>
+      </div>
+    </div>
+  );
+}
