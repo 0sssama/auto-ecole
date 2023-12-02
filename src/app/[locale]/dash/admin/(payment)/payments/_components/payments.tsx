d4b2bf0c09641d66de1338fcf37fbbd49df483c3
Oussama Labrahmi <labrahmioussama@gmail.com>
2023-12-02 15:41:27 +0100

    chore: [AUTO-35] init payments page

---
+++ src/app/[locale]/dash/admin/(payment)/payments/_components/payments.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  PageContentHeader,
+  // AddPaymentsModal
+} from "@/components/molecules";
+import { useModal } from "@/lib/hooks/useModal";
+// import { PaymentsListTable } from "@/components/sections/payments";
+
+const PageHeader = ({ openModal }: { openModal: () => void }) => {
+  const t = useTranslations("Dashboard.Payment.Payments.Header");
+
+  return (
+    <PageContentHeader title={t("title")}>
+      <div className="flex items-center">
+        <Button onClick={openModal}>
+          <Plus size={18} />
+          <span className="hidden ml-2 lg:block">{t("button")}</span>
+        </Button>
+      </div>
+    </PageContentHeader>
+  );
+};
+
+export default function PaymentsPage() {
+  const addPaymentsModal = useModal();
+
+  return (
+    <main>
+      <PageHeader openModal={addPaymentsModal.open} />
+      {/* <AddPaymentsModal
+        isOpen={addPaymentsModal.isOpen}
+        close={addPaymentsModal.close}
+      /> */}
+      <div className="w-full">{/* <PaymentsListTable /> */}</div>
+    </main>
+  );
+}
