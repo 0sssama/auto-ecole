8339ce3ee44250168ac6c47750b971243391829a
Oussama Labrahmi <labrahmioussama@gmail.com>
2023-12-03 16:36:23 +0100

    chore: [AUTO-37] optimize pagination for mobile

--- src/components/organisms/data-table/pagination.tsx
+++ src/components/organisms/data-table/pagination.tsx
@@ -25,17 +25,17 @@ const DataTablePagination: DataTablePaginationComponentType = ({
   const t = useTranslations("Dashboard.Tables.Pagination");
 
   return (
-    <div className="flex items-center justify-end px-2">
-      <div className="flex items-center space-x-6 lg:space-x-8">
-        <div className="flex items-center space-x-2">
-          <p className="text-sm font-medium">{t("rows-per-page")}</p>
+    <div className="flex items-end justify-end gap-2 px-2">
+      <div className="flex items-end justify-end w-full gap-x-2 lg:gap-x-8">
+        <div className="flex items-center gap-x-2 ">
+          <p className="text-xs font-medium md:text-sm">{t("rows-per-page")}</p>
           <Select
             value={`${pagination.get.pageSize}`}
             onValueChange={(value) => {
               pagination.set.pageSize(Number(value));
             }}
           >
-            <SelectTrigger className="h-8 w-[70px]">
+            <SelectTrigger className="h-8 w-[60px] max-w-[70px] text-xs md:text-sm">
               <SelectValue placeholder={pagination.get.pageSize} />
             </SelectTrigger>
             <SelectContent>
@@ -47,50 +47,52 @@ const DataTablePagination: DataTablePaginationComponentType = ({
             </SelectContent>
           </Select>
         </div>
-        <div className="flex w-[100px] items-center justify-center text-sm font-medium">
-          {`${t("page")} ${pagination.get.pageIndex + 1} ${t("of")} ${
-            pagination.get.pageCount
-          }`}
-        </div>
-        <div className="flex items-center space-x-2">
-          <Button
-            variant="outline"
-            className="hidden w-8 h-8 p-0 lg:flex"
-            onClick={() => pagination.set.pageIndex(0)}
-            disabled={!pagination.helpers.canGetPreviousPage()}
-          >
-            <span className="sr-only">{t("go-to-first-page")}</span>
-            <ChevronsLeft className="w-4 h-4" />
-          </Button>
-          <Button
-            variant="outline"
-            className="w-8 h-8 p-0"
-            onClick={() => pagination.helpers.previousPage()}
-            disabled={!pagination.helpers.canGetPreviousPage()}
-          >
-            <span className="sr-only">{t("go-to-previous-page")}</span>
-            <ChevronLeftIcon className="w-4 h-4" />
-          </Button>
-          <Button
-            variant="outline"
-            className="w-8 h-8 p-0"
-            onClick={() => pagination.helpers.nextPage()}
-            disabled={!pagination.helpers.canGetNextPage()}
-          >
-            <span className="sr-only">{t("go-to-next-page")}</span>
-            <ChevronRightIcon className="w-4 h-4" />
-          </Button>
-          <Button
-            variant="outline"
-            className="hidden w-8 h-8 p-0 lg:flex"
-            onClick={() =>
-              pagination.set.pageIndex(pagination.get.pageCount - 1)
-            }
-            disabled={!pagination.helpers.canGetNextPage()}
-          >
-            <span className="sr-only">{t("go-to-last-page")}</span>
-            <ChevronsRight className="w-4 h-4" />
-          </Button>
+        <div className="flex flex-col items-center justify-end sm:!flex-row gap-x-2 gap-y-2">
+          <div className="flex items-center justify-end text-xs font-medium text-right md:text-sm">
+            {`${t("page")} ${pagination.get.pageIndex + 1} ${t("of")} ${
+              pagination.get.pageCount
+            }`}
+          </div>
+          <div className="flex items-end gap-x-2">
+            <Button
+              variant="outline"
+              className="hidden w-8 h-8 p-0 lg:flex"
+              onClick={() => pagination.set.pageIndex(0)}
+              disabled={!pagination.helpers.canGetPreviousPage()}
+            >
+              <span className="sr-only">{t("go-to-first-page")}</span>
+              <ChevronsLeft className="w-4 h-4" />
+            </Button>
+            <Button
+              variant="outline"
+              className="w-8 h-8 p-0"
+              onClick={() => pagination.helpers.previousPage()}
+              disabled={!pagination.helpers.canGetPreviousPage()}
+            >
+              <span className="sr-only">{t("go-to-previous-page")}</span>
+              <ChevronLeftIcon className="w-4 h-4" />
+            </Button>
+            <Button
+              variant="outline"
+              className="w-8 h-8 p-0"
+              onClick={() => pagination.helpers.nextPage()}
+              disabled={!pagination.helpers.canGetNextPage()}
+            >
+              <span className="sr-only">{t("go-to-next-page")}</span>
+              <ChevronRightIcon className="w-4 h-4" />
+            </Button>
+            <Button
+              variant="outline"
+              className="hidden w-8 h-8 p-0 lg:flex"
+              onClick={() =>
+                pagination.set.pageIndex(pagination.get.pageCount - 1)
+              }
+              disabled={!pagination.helpers.canGetNextPage()}
+            >
+              <span className="sr-only">{t("go-to-last-page")}</span>
+              <ChevronsRight className="w-4 h-4" />
+            </Button>
+          </div>
         </div>
       </div>
     </div>
