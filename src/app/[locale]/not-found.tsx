7d9916f7aba9954e7396e2f1edf957a69b6433f3
Oussama Labrahmi <labrahmioussama@gmail.com>
2023-12-23 16:30:07 +0100

    chore: [AUTO-10] edit `not-found` layout

--- src/app/[locale]/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex flex-col items-center justify-center w-full h-screen">
-      <h1 className=" text-6xl font-bold mb-2">404</h1>
-      <p className="text-sm">Page not found</p>
-    </div>
-  );
-}
