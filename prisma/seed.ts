b2d13ec98ba463cc00e38bdc9a48866bcbef4b45
Oussama Labrahmi <labrahmioussama@gmail.com>
2024-01-03 21:22:57 +0100

    hotfix: database schema issue in production

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-import { seedSuperAdmin, seedSecretary } from './seed/seed-super-admin';
+import { seedSuperAdmins, seedSecretaries } from './seed/seed-super-admins';
 import { seedStudents } from './seed/seed-students';
 import { seedInstructors } from './seed/seed-instructors';
 import { seedLicenseFiles } from './seed/seed-license-files';
@@ -8,8 +8,19 @@ import { seedSchool } from './seed/seed-school';
 import { seedCashFund } from './seed/seed-cash-fund';
 
 export const clerkOrgId = 'org_2Vr89LWYt1FRrhR7ALcBq5HqYhC';
-export const superAdminClerkId = 'user_2YBRtRz9u0fyELr5fus2ty0aOeo';
-export const secretaryClerkId = 'user_2ZMBck8CD5AsqoewQZoLCbwFhU7';
+export const superAdminClerkIds = [
+  {
+    username: 'ecpp_super_admin',
+    fullName: 'ECPP SUPER ADMIN',
+    id: 'user_2YBRtRz9u0fyELr5fus2ty0aOeo',
+  },
+  {
+    username: 'osm_super_admin',
+    fullName: 'OSM SUPER ADMIN',
+    id: 'user_2YPTIqDuUclX7PcPkZJeRIyhHTv',
+  },
+];
+export const secretaryClerkIds = ['user_2ZMBck8CD5AsqoewQZoLCbwFhU7'];
 
 const twirlTimer = () => {
   const P = ['\\', '|', '/', '-'];
@@ -36,15 +47,29 @@ async function main() {
     console.timeEnd('Seeding Cash Fund');
 
     console.time('Seeding SuperAdmin');
-    const superAdmin = await seedSuperAdmin(prisma);
+    const superAdmins = await seedSuperAdmins(prisma);
     console.timeEnd('Seeding SuperAdmin');
 
     console.time('Seeding Secretary');
-    await seedSecretary(prisma);
+    await seedSecretaries(prisma);
     console.timeEnd('Seeding Secretary');
 
+    // ask user if they want to continue seeding, if not stop
+    const readline = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    readline.question(`Continue seeding? (press 'n' if we are in PROD) (y/n) `, (answer: string) => {
+      if (answer === 'n') {
+        console.log('Stopping seeding...');
+        process.exit(0);
+      }
+      readline.close();
+    });
+
     console.time('Seeding Students');
-    const students = await seedStudents(prisma, superAdmin.id);
+    const students = await seedStudents(prisma, superAdmins[0].id);
     console.timeEnd('Seeding Students');
 
     console.time('Seeding Instructors');
@@ -56,7 +81,7 @@ async function main() {
       prisma,
       instructors.map((instructor) => instructor.id),
       students.map((student) => student.id),
-      superAdmin.id,
+      superAdmins[0].id,
       cashFund.id,
     );
     console.timeEnd('Seeding LicenseFiles');
